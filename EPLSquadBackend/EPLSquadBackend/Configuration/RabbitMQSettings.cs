e5b393eeb6311c258aabe5ff9383f2eca16cb0f7
VKulyck <vital29115@gmail.com>
2025-02-03 23:28:49 +0200

    Added RabbitMQ files

---
+++ EPLSquadBackend/EPLSquadBackend/Configuration/RabbitMQSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace EPLSquadBackend.Configuration
+{
+    public class RabbitMQSettings
+    {
+        public string Hostname { get; set; } = "rabbitmq";
+        public int Port { get; set; } = 5672;
+        public string QueueName { get; set; } = "EPLSquadQueue";
+        public string Username { get; set; } = "guest";
+        public string Password { get; set; } = "guest";
+    }
+}
---
+++ EPLSquadBackend/EPLSquadBackend/Services/RabbitMQService.cs
@@ -0,0 +1,47 @@
+﻿using EPLSquadBackend.Configuration;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace EPLSquadBackend.Services
+{
+    public class RabbitMQService
+    {
+        private readonly RabbitMQSettings _rabbitSettings;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public RabbitMQService(IOptions<RabbitMQSettings> rabbitOptions)
+        {
+            _rabbitSettings = rabbitOptions.Value;
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = _rabbitSettings.Hostname,
+                Port = _rabbitSettings.Port,
+                UserName = _rabbitSettings.Username,
+                Password = _rabbitSettings.Password
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: _rabbitSettings.QueueName,
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+        }
+
+        public void PublishMessage<T>(T message)
+        {
+            var json = JsonSerializer.Serialize(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            _channel.BasicPublish(exchange: "",
+                                  routingKey: _rabbitSettings.QueueName,
+                                  basicProperties: null,
+                                  body: body);
+        }
+    }
+}
